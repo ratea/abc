b1803c18eeb4f454afc4effd562b998ff8eefa97
genki.t <roma3983@gmail.com>
2017-05-14 15:25:40 +0900

    Add files via upload

---
+++ abc54/b.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main(){
+	int n,m;
+	cin>>n>>m;
+	vector<string> a(n);
+	vector<string> b(m);
+	
+	for(int i = 0;i<n;i++)cin>>a[i];
+	for(int i = 0;i<m;i++)cin>>b[i];
+	
+	int ans;
+	for(int i = 0;i<n-m+1;i++){
+		for(int l = 0;l<n-m+1;l++){
+			ans=1;
+			for(int j = 0;j<m;j++){
+				for(int k = 0;k<m;k++){
+					if(a[i+j][l+k]!=b[j][k]){
+						ans=0;
+					}
+				}
+				if(ans==1){
+				cout<<"Yes"<<endl;
+				return 0;
+				}
+			}
+		}
+	}
+	cout<<"No"<<endl;
+	return 0;
+} 
