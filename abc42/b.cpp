647e78692d92cf3809470cacc30c5e8902aa8188
genki.t <roma3983@gmail.com>
2017-05-16 00:52:09 +0900

    Add files via upload

--- abc42/b.cpp
+++ abc42/b.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<set>
+#include<map>
 
 int main(){
 	int n,l;
 	cin>>n>>l;
-	vector<string> s(n);
+	map<string,int> s;
 	for(int i = 0;i<n;i++){
-		cin>>s[i];
+		string a;
+		cin>>a;
+		s[a]++;
 	}
 	
+	for(auto &e:s){
+		for(int i = 0;i < e.second;i++){
+			cout<<e.first;
+		}
+	}
+	cout<<endl;
+	
+	return 0;
+}
