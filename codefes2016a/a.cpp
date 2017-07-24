a08b8f9cfd1db4807f534d40f4d5c0945d26a134
genki.t <roma3983@gmail.com>
2017-07-25 02:31:39 +0900

    Add files via upload

---
+++ codefes2016a/a.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+	for(int i=0;i<4;i++)cout<<s[i];
+	cout<<" ";
+	for(int i=4;i<12;i++)cout<<s[i];
+	cout<<endl;
+	return 0;
+}
