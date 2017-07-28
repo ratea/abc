ff896c91a24e7db972b90706202ced5154705ba2
genki.t <roma3983@gmail.com>
2017-07-29 03:04:40 +0900

    Add files via upload

---
+++ codefes2016a/b.cpp
@@ -0,0 +1,19 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+		a[i]--;
+	}
+	int ans=0;
+	for(int i=0;i<n;i++){
+		if(i==a[a[i]])ans++;
+	}
+	
+	cout<<ans/2<<endl;
+	return 0;
+}
