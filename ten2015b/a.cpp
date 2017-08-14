aa2892d7bfca878e45e018f48e0b30b28a7d5a87
genki.t <roma3983@gmail.com>
2017-08-15 00:50:37 +0900

    Add files via upload

--- ten2015b/a.cpp
+++ ten2015b/a.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int main(){
+  vector<int> a(20);
+  a[0]=100;
+  a[1]=100;
+  a[2]=200;
+  for(int i=3;i<20;i++){
+    a[i]=a[i-1]+a[i-2]+a[i-3];
+  }
+  cout<<a[19]<<endl;
+  return 0;
+}
