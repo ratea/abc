c8fc84f6f10c305db80894505d71e02235db562d
genki.t <roma3983@gmail.com>
2017-08-16 02:10:16 +0900

    Add files via upload

--- arc/arc060/a.cpp
+++ arc/arc060/a.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+#include<vector>
 
+int main(){
+  int n,a;
+  cin>>n>>a;
+  vector<int> x(n);
+  for(int i=0;i<n;i++){
+    cin>>x[i];
+    x[i]-=a;
+  }
+  //vector< vector< vector<int> > > dp(n,vector< vector<int> >(n,vector<int>(n,0)));
+
+}
