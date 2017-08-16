c6b25dd89f64e58b357ad26c1108abc61d5ee1f6
genki.t <roma3983@gmail.com>
2017-08-16 13:41:18 +0900

    Add files via upload

--- arc/arc060/a2.cpp
+++ arc/arc060/a2.cpp
@@ -6,24 +6,24 @@ int main(){
   long long int n,a;
   cin>>n>>a;
   vector<long long int> x(n);
+  long long int mx=0;
   for(int i=0;i<n;i++){
     cin>>x[i];
+    mx=max(mx,x[i]);
     x[i]-=a;
   }
   //vector< vector< vector<int> > > dp(n,vector< vector<int> >(n,vector<int>(n,0)));
-  vector<vector<long long int> > dp(n+1,vector<long long int>(n*n*2,0));
+  vector<vector<long long int> > dp(n+1,vector<long long int>(n*mx*2,0));
 
-  dp[0][n*n]=1;
+  dp[0][n*mx]=1;
 
   for(int i=1;i<n+1;i++){
     //cout<<x[i]<<endl;
-    for(int j=0;j<n*n*2;j++){
-      dp[i][j]=dp[i-1][j]+dp[i-1][j-x[i-1]];
-      // dp[i+1][j+(n*n)]+=dp[i][j+(n*n)];
-      // dp[i+1][j+x[i]+(n*n)]+=dp[i][j+(n*n)];
+    for(int j=0;j<n*mx*2;j++){
+        dp[i][j]=dp[i-1][j]+dp[i-1][j-x[i-1]];
     }
   }
 
-  cout<<dp[n][n*n]-1<<endl;
+  cout<<dp[n][n*mx]-1<<endl;
   return 0;
 }
