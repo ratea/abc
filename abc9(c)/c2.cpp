9e57803b0c601426104b3269f0bd2925ea36e7fa
genki.t <roma3983@gmail.com>
2017-09-19 00:52:54 +0900

    Add files via upload

---
+++ abc9(c)/c2.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	int n,k;
+	cin>>n>>k;
+	string s;
+	cin>>s;
+	vector<int> used(n,0);
+	vector<char> ch(k);
+	set<char> 
+	
+	int scount=0;
+	
+	
