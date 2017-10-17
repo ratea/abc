147967009248978badbd2347da3a93c3b38ea6c9
genki.t <roma3983@gmail.com>
2017-10-18 00:19:56 +0900

    Add files via upload

---
+++ arc/arc072/b.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	long long int x,y;
+	cin>>x>>y;
+	if(abs(x-y)>1){
+		cout<<"Alice"<<endl;
+	}else{
+		cout<<"Brown"<<endl;
+	}
+	
+	return 0;
+}
