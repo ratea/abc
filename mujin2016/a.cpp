64b4af0871891df622c9546900c80120f30b45f0
genki.t <roma3983@gmail.com>
2017-12-03 01:53:13 +0900

    Add files via upload

---
+++ mujin2016/a.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	char a;
+	cin>>a;
+	
+	if(a=='O'||a=='L'||a=='K'||a=='P'){
+		cout<<"Right"<<endl;
+	}else{
+		cout<<"Left"<<endl;
+	}
+	
+	return 0;
+}
