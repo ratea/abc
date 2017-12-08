7ed22f743f42f1a7239d4851a1d84edd79a7870a
genki.t <roma3983@gmail.com>
2017-12-09 05:18:20 +0900

    Add files via upload

---
+++ codefes2015f/a.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+	string s,t,u;
+	cin>>s>>t>>u;
+	if(s.size()==5&&t.size()==7&&u.size()==5){
+		cout<<"valid"<<endl;
+	}else{
+		cout<<"invalid"<<endl;
+	}
+	return 0;
+}
