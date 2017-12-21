b3eec83c030ea16e95332d38c119b30ea9e61476
genki.t <roma3983@gmail.com>
2017-12-22 04:01:14 +0900

    Add files via upload

---
+++ abc80/c - コピー.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+vector<vector<int> > f(101,vector<int>(10));
+vector<vector<int> > p(101,vector<int>(10));
+
+vector<int> aki(101,0);
+
+int dfs(){
+	
+	
+	
+	
+}
+
+int main(){
+	int n;
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<10;j++)cin>>f[i][j];
+	}
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<10;j++)cin>>p[i][j];
+	}
+	
+	int ans=0;
+	
+	
+	
+	
+	return 0;
+}
--- abc80/c.cpp
+++ abc80/c.cpp
@@ -1,17 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-vector<vector<int> > f(101,vector<int>(10));
-vector<vector<int> > p(101,vector<int>(10));
-
-vector<int> aki(101,0);
-
-int dfs(){
-	
-	
-	
-	
-}
+vector<vector<int> > f(101,vector<int>(101));
+vector<vector<int> > p(101,vector<int>(101));
 
 int main(){
 	int n;
@@ -22,13 +13,24 @@ int main(){
 	}
 	
 	for(int i=0;i<n;i++){
-		for(int j=0;j<10;j++)cin>>p[i][j];
+		for(int j=0;j<11;j++)cin>>p[i][j];
 	}
 	
-	int ans=0;
+	int ans=-1e9;
 	
-	
-	
-	
+	for(int i=1;i<(1<<10);i++){
+		int tmp=0;
+		for(int j=0;j<n;j++){
+			int x=0;
+			for(int k=0;k<10;k++){
+				if((i>>k&1)&&f[j][k])x++;
+			}
+			tmp+=p[j][x];
+		}
+		//cout<<tmp<<endl;
+		//if(ans<tmp)cout<<i<<" "<<tmp<<" "<<ans<<endl;
+		ans=max(ans,tmp);
+	}
+	cout<<ans<<endl;
 	return 0;
 }
